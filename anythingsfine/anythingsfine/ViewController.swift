c807045d8ebb5efd3c61330ea30bdb03548cf998
Mely Bohlman <melykiwi@dhcp-10-134-224-108.uawifi.arizona.edu>
2018-10-24 13:57:03 -0700

    First commit

---
+++ anythingsfine/anythingsfine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  anythingsfine
+//
+//  Created by Mely Bohlman on 10/24/18.
+//  Copyright © 2018 Chris Bohlman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
