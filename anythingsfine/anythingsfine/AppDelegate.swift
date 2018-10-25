e6fa13c24d3080697033ce875fd9088d9ee91a17
DennyHo2020 <dennyho@email.arizona.edu>
2018-10-25 12:53:36 -0700

    login

--- anythingsfine/anythingsfine/AppDelegate.swift
+++ anythingsfine/anythingsfine/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initialize(with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) in
+            configuration.applicationId = "AnythingIsFine"
+            configuration.clientKey = "gfxre3q23s45r56tg78uj9in9ubuyft6cc"
+            configuration.server = "http://boiling-island-19714.herokuapp.com/parse"
+        }))
+        // check if user is logged in.
+        if PFUser.current() != nil {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            // view controller currently being set in Storyboard as default will be overridden
+            window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController")
+        }
+        NotificationCenter.default.addObserver(forName: Notification.Name("didLogout"), object: nil, queue: OperationQueue.main) { (Notification) in
+            print("Logout notification received")
+            self.logOut()
+            // TODO: Load and show the login view controller
+        }
+        
         return true
+    }
+    
+    // Logout the current user
+    func logOut() {
+        PFUser.logOutInBackground(block: { (error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("Successful loggout")
+                // Load and show the login view controller
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let loginViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+                self.window?.rootViewController = loginViewController
+            }
+        })
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- anythingsfine/anythingsfine/HomeViewController.swift
+++ anythingsfine/anythingsfine/HomeViewController.swift
@@ -1,24 +1,23 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  anythingsfine
 //
-//  Created by Denny Ho on 10/24/18.
+//  Created by Denny Ho on 10/25/18.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
-
