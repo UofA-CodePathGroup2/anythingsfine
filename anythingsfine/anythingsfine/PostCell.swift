486dbf84e363a19f87af8291e68e3a7d7af1d524
DennyHo2020 <dennyho@email.arizona.edu>
2018-11-03 16:06:45 -0700

    stop

---
+++ anythingsfine/anythingsfine/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  anythingsfine
+//
+//  Created by Denny Ho on 11/3/18.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
